dc442aacd7a2b6658ee489fcda7fd51bf27c62a7
kasmaniziad <kasmaniziad@outlook.com>
2016-12-28 13:20:50 +0300

    ZResource

---
+++ JWT/ZResource/Helpers/CodeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZResource.Helpers
+{
+    public class CodeHelper
+    {
+
+        public const string UnableToGetFiles = "UnableToGetFiles";
+
+        public const string UnableToAddFile = "UnableToAddFile";
+
+        public const string UnableToDeleteFile = "UnableToDeleteFile";
+
+        public const string UnableToFindFolder = "UnableToFindFolder";
+
+    }
+}
---
+++ JWT/ZResource/Manager/AppConfManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+
+namespace ZResource.Managers
+{
+    public class AppConfManager
+    {
+        public static string ConnectionString { get { return "ConnectionString"  ; } }
+        
+    }
+}
